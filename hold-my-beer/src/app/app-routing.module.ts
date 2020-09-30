0435a78d38091065a86a565e9469f75a66711679
ClementLegouest <clement.legouest@gmail.com>
2020-09-30 17:03:52 +0200

    [evo] Bulma Welcome page

--- hold-my-beer/src/app/app-routing.module.ts
+++ hold-my-beer/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {WelcomeComponent} from './welcome/component/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'index', component: WelcomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
