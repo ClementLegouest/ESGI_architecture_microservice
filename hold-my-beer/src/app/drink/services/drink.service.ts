d5cc1fce3b6f0e2b17597a5a5bac41ebc6ffa95d
ClementLegouest <clement.legouest@gmail.com>
2020-10-01 12:26:42 +0200

    drink service added

---
+++ hold-my-beer/src/app/drink/services/drink.service.ts
@@ -0,0 +1,23 @@
+import {Injectable, OnInit} from '@angular/core';
+import {Drink} from '../models/drink';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DrinkService {
+
+  drinks: Drink[] = [
+    new Drink('1', 'lagger', true),
+    new Drink('2', 'Limonade', false),
+    new Drink('3', 'Stout', true),
+    new Drink('4', 'Jus de tomate', false),
+    new Drink('5', 'Calvados', true)
+  ];
+
+  constructor() { }
+
+  // STUB to delete
+  getDrinksFromLocal(): Drink[] {
+    return this.drinks;
+  }
+}
