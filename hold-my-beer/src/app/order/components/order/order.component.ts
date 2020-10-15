3f39286a47fd40f3ff6684e12cbcc0efb8606c4d
ClementLegouest <clement.legouest@gmail.com>
2020-10-15 15:08:09 +0200

    récap commande

--- hold-my-beer/src/app/order/components/order/order.component.ts
+++ hold-my-beer/src/app/order/components/order/order.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {DrinkService} from '../../../drink/services/drink.service';
 import {Drink} from '../../../drink/models/drink';
+import {OrderService} from '../../services/order.service';
 
 @Component({
   selector: 'app-order',
@@ -11,9 +12,14 @@ export class OrderComponent implements OnInit {
 
   drinks: Drink[] = new Array<Drink>();
 
-  constructor(private drinkService: DrinkService) { }
+  constructor(private drinkService: DrinkService,
+              private orderService: OrderService) { }
 
   ngOnInit(): void {
     this.drinks = this.drinkService.getDrinksFromLocal();
+  }
+
+  drinkNameById(drinkId: number): string {
+    return this.drinks.find((drink) => drink.uid === drinkId.toString()).name;
   }
 }
--- hold-my-beer/src/app/order/services/order.service.ts
+++ hold-my-beer/src/app/order/services/order.service.ts
@@ -10,10 +10,19 @@ export class OrderService {
 
   constructor() { }
 
+  getOrders(): Order[] {
+    return this.orders;
+  }
+
   addADrink(drinkId: number, quantity: number): void {
     if (quantity > 0) {
       this.orders.push(new Order(drinkId, quantity, new Date()));
     }
+    console.log(this.orders);
+  }
+
+  sendOrder(): void {
+    console.log('Commande envoyée : ');
     console.log(this.orders);
   }
 }
